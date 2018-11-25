0c9c1972b63bd44b87cfdff76a62c6e45d446977
kenzo0107 <kenzo.tanaka0107@gmail.com>
2018-11-25 13:53:25 +0900

    :gear: VAPID 利用し ServiceWorker Web プッシュ導入

--- app/assets/javascripts/serviceworker-companion.js
+++ app/assets/javascripts/serviceworker-companion.js
@@ -1,6 +1,139 @@
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
-    .then(function(reg) {
-      console.log('[Companion]', 'Service worker registered!');
+'use string';
+
+const applicationServerKey = window.vapidPublicKey;
+
+let pushButton;
+let isSubscribed = false;
+let swRegistration = null;
+
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push is supported');
+
+  // navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
+
+  navigator.serviceWorker.register('/serviceworker.js')
+    .then(function(swReg) {
+      console.log('[Companion]', 'Service worker registered!', swReg);
+
+      swRegistration = swReg;
+
+      pushButton = document.querySelector('.js-push-btn');
+      initialiseUI();
+    })
+    .catch(function(error) {
+      console.error('Service Worker Error', error);
     });
+} else {
+  console.warn('Push messaging is not supported');
+  pushButton.textContent = 'Push Not Supported';
+}
+
+function initialiseUI() {
+  if(!pushButton) {
+    console.log('no push button exist.')
+    return;
+  }
+
+  pushButton.addEventListener('click', function() {
+    pushButton.disabled = true;
+    if (isSubscribed) {
+      unsubscribeUser();
+    } else {
+      subscribeUser();
+    }
+  });
+
+  // Set the initial subscription value
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    isSubscribed = !(subscription === null);
+
+    if (isSubscribed) {
+      console.log('User IS subscribed.');
+    } else {
+      console.log('User is NOT subscribed.');
+    }
+
+    updateBtn();
+  });
+}
+
+function subscribeUser() {
+  // const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+  swRegistration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: applicationServerKey
+  })
+  .then(function(subscription) {
+    console.log('User is subscribed:', subscription);
+
+    updateSubscriptionOnServer(subscription);
+
+    isSubscribed = true;
+
+    updateBtn();
+  })
+  .catch(function(err) {
+    console.log('Failed to subscribe the user: ', err);
+    updateBtn();
+  });
+}
+
+function unsubscribeUser() {
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    if (subscription) {
+      return subscription.unsubscribe();
+    }
+  })
+  .catch(function(error) {
+    console.log('Error unsubscribing', error);
+  })
+  .then(function() {
+    updateSubscriptionOnServer(null);
+
+    console.log('User is unsubscribed.');
+    isSubscribed = false;
+
+    updateBtn();
+  });
+}
+
+function updateBtn() {
+  if (Notification.permission === 'denied') {
+    pushButton.textContent = 'プッシュ通知をブロックしています';
+    pushButton.disabled = true;
+    // updateSubscriptionOnServer(null);
+    return;
+  }
+
+  if (isSubscribed) {
+    pushButton.textContent = 'プッシュ通知を無効にする';
+  } else {
+    pushButton.textContent = 'プッシュ通知を有効にする';
+  }
+
+  pushButton.disabled = false;
+}
+
+function updateSubscriptionOnServer(subscription) {
+  console.log(subscription);
+  var params = {
+    subscription: subscription.toJSON()
+  }
+
+  console.log(params);
+
+  // TODO サーバサイドに subscription 情報を POST 処理
+
+  const subscriptionJson = document.querySelector('.js-subscription-json');
+  const subscriptionDetails =
+    document.querySelector('.js-subscription-details');
+
+  if (subscription) {
+    subscriptionJson.textContent = JSON.stringify(subscription);
+    subscriptionDetails.classList.remove('is-invisible');
+  } else {
+    subscriptionDetails.classList.add('is-invisible');
+  }
 }
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  helper_method :vapid_public_key
+  def vapid_public_key
+    vpk = Rails.application.credentials.dig(Rails.env.to_sym, :vapid_public_key)
+    @decoded_vapid_public_key ||= Base64.urlsafe_decode64(vpk).bytes
+  end
 end
